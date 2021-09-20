aedc02bd2770e26c553d6665a1e2807b7841cd4b
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2021-09-20 16:23:37 +0200

    add some logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Web3 from 'web3';
+import { useEffect, useState } from 'react';
+import Button from '@mui/material/Button';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const url = 'https://mainnet.infura.io/v3/e31d6b31abd249718714f1244b8628e7 ';
+	const web3 = new Web3(url);
+	let address = '0x00000000219ab540356cBB839Cbe05303d7705Fa';
+	const [ balance, setBalance ] = useState(0);
+	const howMuch = () => {
+		web3.eth.getBalance <number> (address, (err: any, bal: number) => {
+			setBalance(web3.utils.fromWei(bal, 'ether'));
+		});
+	};
+	useEffect(
+		() => {
+			console.log(typeof balance);
+		},
+		[ balance ]
+	);
+	return (
+		<div>
+			<h1 color="initial">{`${balance}`}</h1>
+			<Button onClick={howMuch}>rich?</Button>
+		</div>
+	);
 }
-
 export default App;

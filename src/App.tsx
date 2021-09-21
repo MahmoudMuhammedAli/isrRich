1f711d86ac914a66fc8fb1a1e9647b98696d09c5
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2021-09-21 22:30:18 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import './App.scss';
 import Web3 from 'web3';
 import { useEffect, useState } from 'react';
-import Button from '@mui/material/Button';
 import { ReactComponent as Header } from './assets/header.svg';
 import { ReactComponent as Logo } from './assets/isrich.svg';
+import { ReactComponent as Happy } from './assets/5867.svg';
+
 import { Modal, Typography } from '@mui/material';
-import { Box } from '@mui/system';
 const url = 'https://mainnet.infura.io/v3/e31d6b31abd249718714f1244b8628e7 ';
 const web3 = new Web3(url);
 
@@ -13,9 +13,11 @@ function App() {
 	const [ address, setAddress ] = useState('');
 	const [ balance, setBalance ] = useState(0);
 	const [ open, setOpen ] = useState(false);
+	const [ error, setError ] = useState(false);
 	async function howMuch() {
 		await web3.eth
 			.getBalance(address, (err: any, bal: string) => {
+				err && setError(true);
 				let temp: number = Number(web3.utils.fromWei(bal, 'ether'));
 				setBalance(temp);
 			})
@@ -33,46 +35,48 @@ function App() {
 	);
 	return (
 		<div className="main-view">
-			<Logo />
+			<Logo className="logo" />
 			<div className="content">
-				<Header />
-
-				<input
-					type="text"
-					name=""
-					id=""
-					placeholder="Public Key"
-					value={address}
-					className="public-key"
-					onChange={changePublicKey}
-					required
-				/>
-				<button type="submit" className="submit" onClick={howMuch} disabled={!address}>
-					rich?
-				</button>
+				<Header className="header" />
+				<div className="inputs">
+					<input
+						className="public-key"
+						type="text"
+						name=""
+						id=""
+						placeholder="Public Key"
+						value={address}
+						onChange={changePublicKey}
+						required
+					/>
+					<button type="submit" className="submit" onClick={howMuch} disabled={!address}>
+						isRich?
+					</button>
+				</div>
 				<Modal
 					open={open}
 					onClose={handleClose}
 					aria-labelledby="modal-modal-title"
 					aria-describedby="modal-modal-description"
 				>
-					<Box
-						sx={{
-							position: 'absolute',
-							top: '50%',
-							left: '50%',
-							transform: 'translate(-50%, -50%)',
-							width: 400,
-							bgcolor: 'background.paper',
-							border: '2px solid #000',
-							boxShadow: 24,
-							p: 4
-						}}
-					>
-						<Typography id="modal-modal-title" variant="h3" component="h2">
-							{balance}
-						</Typography>
-					</Box>
+					{error ? (
+						<h1>Enter a valid pirivate key</h1>
+					) : (
+						<div className="modal">
+							<div className="left">
+								<h1 className="modal-header">{balance > 4 ? 'MF is Rich!🤑' : 'Nope, Poor! 🥺'}</h1>
+
+								<h1 className="title-2">Ether:</h1>
+
+								<h1 className="amount">{balance} Ξ</h1>
+
+								<h1 className="title-2">Dollars:</h1>
+
+								<h1 className="amount">{`${balance * 2912.17} 💲`}</h1>
+							</div>
+							<div className="right" />
+						</div>
+					)}
 				</Modal>
 			</div>
 		</div>

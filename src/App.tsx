81f3c38cbab162b11b167c0aa763462876ef9929
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2021-09-21 15:37:37 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,40 @@ import { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
 import {ReactComponent as Header} from './assets/header.svg'
 import {ReactComponent as Logo} from './assets/isrich.svg'
+const url = 'https://mainnet.infura.io/v3/e31d6b31abd249718714f1244b8628e7 ';
+const web3 = new Web3(url);
 function App() {
-	const url = 'https://mainnet.infura.io/v3/e31d6b31abd249718714f1244b8628e7 ';
-	const web3 = new Web3(url);
-	let address = '0x00000000219ab540356cBB839Cbe05303d7705Fa';
+	const [address, setAddress] = useState("")
 	const [ balance, setBalance  ] = useState(0);
-	const howMuch = () => {
-		web3.eth.getBalance (address, (err: any, bal: string) => {
-			let temp : number = Number(web3.utils.fromWei(bal, 'ether')) 
-			setBalance(temp);
-		});
+	const [open, setOpen] = useState(false);
+	 async function  howMuch  () {
+		await  web3.eth.getBalance (address, (err: any, bal: string) => {
+			let temp : number =  Number(web3.utils.fromWei(bal, 'ether')) 
+			 setBalance(temp)	
+		}).then(setOpen(true))
 	};
+	const changePublicKey = (e : React.FormEvent<HTMLInputElement>)=>{
+		setAddress ( e.currentTarget.value)
+	}
+	const handleClose = () => setOpen(false);
 	useEffect(
 		() => {
-			console.log(typeof balance); 
-		},
+			console.log(balance); 
+		}, 
 		[ balance ]
 	);
 	return (
 		<div className="main-view">
 			<Logo/>
+			<div className="content">
+
 			<Header/> 
-			<Button variant="contained" onClick={howMuch}>rich?</Button>
+		
+				<input type="text" name="" id="" placeholder="Public Key" value = {address}className="public-key" onChange={changePublicKey} required/>
+				<button type="submit" className="submit" onClick={howMuch} disabled={!address} >rich?</button>
+			
+
+			</div>
 		</div>
 	);
 }

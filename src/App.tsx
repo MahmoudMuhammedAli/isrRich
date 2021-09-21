a94a3b4e80b3efd27d3ab4cb02b515525a7ed3c1
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2021-09-21 13:21:22 +0200

    add the assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,31 @@
-import './App.css';
+import './App.scss';
 import Web3 from 'web3';
 import { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
-
+import {ReactComponent as Header} from './assets/header.svg'
+import {ReactComponent as Logo} from './assets/isrich.svg'
 function App() {
 	const url = 'https://mainnet.infura.io/v3/e31d6b31abd249718714f1244b8628e7 ';
 	const web3 = new Web3(url);
 	let address = '0x00000000219ab540356cBB839Cbe05303d7705Fa';
-	const [ balance, setBalance ] = useState(0);
+	const [ balance, setBalance  ] = useState(0);
 	const howMuch = () => {
-		web3.eth.getBalance <number> (address, (err: any, bal: number) => {
-			setBalance(web3.utils.fromWei(bal, 'ether'));
+		web3.eth.getBalance (address, (err: any, bal: string) => {
+			let temp : number = Number(web3.utils.fromWei(bal, 'ether')) 
+			setBalance(temp);
 		});
 	};
 	useEffect(
 		() => {
-			console.log(typeof balance);
+			console.log(typeof balance); 
 		},
 		[ balance ]
 	);
 	return (
-		<div>
-			<h1 color="initial">{`${balance}`}</h1>
-			<Button onClick={howMuch}>rich?</Button>
+		<div className="main-view">
+			<Logo/>
+			<Header/> 
+			<Button variant="contained" onClick={howMuch}>rich?</Button>
 		</div>
 	);
 }
